02462af1fb17ffa4e06edcab52c0a251ad7239e0
Taran Dunn <tdunn@organikweb.com.au>
2020-11-21 15:35:18 +0800

    Shortcode output updated

--- public/shortcode/shortcode.php
+++ public/shortcode/shortcode.php
@@ -18,16 +18,18 @@ if ( $testimonials_loop->have_posts() ) : ?>
             while ( $testimonials_loop->have_posts() ) : $testimonials_loop->the_post();
 
                 // Variables
-                $position = esc_html( get_post_meta( get_the_ID(), '_testimonial_position', true ) );
-                $company = esc_html( get_post_meta( get_the_ID(), '_testimonial_company', true ) );
+                $name = esc_html( get_the_title() );
+                $position = esc_html( get_post_meta( get_the_ID(), 'testimonial_position', true ) );
+                $company = esc_html( get_post_meta( get_the_ID(), 'testimonial_company', true ) );
+                $attribute = array( $name, $position, $company );
 
                 // Only display the testimonial if it has a quote (content) set
                 if ( get_the_content() ) : ?>
 
                     <div <?php post_class( $entry_class ) ?>>
                         <blockquote>
-                            <p><?php echo wpautop( wp_kses_post( get_the_content() ) ) ?></p>
-                            <p><?php echo esc_html( the_title() ) ?></p>
+                            <?php echo wpautop( wp_kses_post( get_the_content() ) ) ?>
+                            <p class="attribute"><?php echo implode( ', ', array_filter( $attribute ) ) ?></p>
                         </blockquote>
                     </div>
                 <?php endif; 
